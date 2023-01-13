3604d9c678e9bb64cea2058bc86f32ffa5f5884f
Mouctar <47394031+mamadou-mouctar-diallo@users.noreply.github.com>
2023-01-13 03:02:03 +0100

    commit add config env variables

--- src/environments/environment.deloynment.ts
+++ src/environments/environment.deloynment.ts
@@ -2,11 +2,9 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8015/api',
   envVar: {
-    /**
-     * Add environment variables you want to retrieve from process
+    /** * Add environment variables you want to retrieve from process
      * PORT:4200,
-     * VAR_NAME: defaultValue
-     */
+     * VAR_NAME: defaultValue */
     endpointUrl: 'https://api-backend-ewpe8banv-mamadou-mouctar-diallo.vercel.app/api',
     LOG_LEVEL: 'debug',
     verison: 0
